7562e33553c9e577f41868cddd5a1f3200b90823
Airsave <hooke1027@naver.com>
2024-08-18 10:43:19 +0900

    Style : Pororo image added, User MessageCard added

--- app/src/main/java/com/example/sunflower/MainActivity.kt
+++ app/src/main/java/com/example/sunflower/MainActivity.kt
@@ -27,13 +27,16 @@ import androidx.compose.runtime.setValue
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.ui.res.painterResource
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.RectangleShape
 import androidx.compose.ui.unit.dp
+
 import androidx.compose.ui.tooling.preview.Preview
 import android.content.res.Configuration
 import com.example.sunflower.ui.theme.SunflowerTheme
@@ -54,34 +57,45 @@ class MainActivity : ComponentActivity() {
     }
 }
 
-data class Message(val author: String, val body: String)
+data class Message(
+    val author: String,
+    val body: String,
+    val imageResourceId: Int?= null
+)
 
 @Composable
 fun MessageCard(msg: Message) {
     // Add padding around our message
-    Row (modifier = Modifier.padding(all = 8.dp)) {
-        Image(
-            painter = painterResource(R.drawable.profile_picture),
-            contentDescription = "Contact profile picture",
-            modifier = Modifier
-                // Set image size to 40 dp
-                .size(40.dp)
-                //clip image to be shaped as a circle
-                .clip(CircleShape)
-                //set image border
-                .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
-        )
+    Row (
+        modifier = Modifier
+            .fillMaxWidth()
+            .padding(all = 8.dp),
+        horizontalArrangement = if (msg.author == "User") Arrangement.End else Arrangement.Start) {
+        if (msg.author != "User") {
+            Image(
+                painter = painterResource(R.drawable.profile_picture),
+                contentDescription = "Contact profile picture",
+                modifier = Modifier
+                    // Set image size to 40 dp
+                    .size(40.dp)
+                    //clip image to be shaped as a circle
+                    .clip(CircleShape)
+                    //set image border
+                    .border(1.5.dp, MaterialTheme.colorScheme.primary, CircleShape)
+            )
 
-        // Add a horizontal space between the image and the column
-        Spacer(modifier = Modifier.width(8.dp))
-
+            // Add a horizontal space between the image and the column
+            Spacer(modifier = Modifier.width(8.dp))
+        }
         // We keep track if the message is expanded or not in this variable
+        /*
         var isExpanded by remember {mutableStateOf(false)}
         val surfaceColor by animateColorAsState(
             if (isExpanded) MaterialTheme.colorScheme.primary else MaterialTheme.colorScheme.surface,
         )
+         */
         // We toggle the isExpanded variable when we click on this Column
-        Column (modifier = Modifier.clickable {isExpanded = !isExpanded}){
+        Column {
             Text(
                 text = msg.author,
                 color = MaterialTheme.colorScheme.secondary,
@@ -93,15 +107,29 @@ fun MessageCard(msg: Message) {
             Surface(
                 shape = MaterialTheme.shapes.medium,
                 shadowElevation = 1.dp,
-                color = surfaceColor,
+                color = if (msg.author == "User") {
+                    MaterialTheme.colorScheme.primary
+                } else {
+                    MaterialTheme.colorScheme.surface
+                },
                 modifier = Modifier.animateContentSize().padding(1.dp)
             ) {
-                Text(
-                    text = msg.body,
-                    modifier = Modifier.padding(all = 4.dp),
-                    maxLines = if (isExpanded) Int.MAX_VALUE else 1,
-                    style = MaterialTheme.typography.bodyMedium
-                )
+                Column(modifier = Modifier.padding(all = 8.dp)) {
+                    if (msg.imageResourceId != null) {
+                        //add Image for message with padding
+                        Image(
+                            painter = painterResource(msg.imageResourceId),
+                            contentDescription = "Message Image",
+                            modifier = Modifier.padding(bottom = 4.dp)
+                        )
+                    }
+                    Text(
+                        text = msg.body,
+                        modifier = Modifier.padding(all = 4.dp),
+                        //maxLines = if (isExpanded) Int.MAX_VALUE else 1,
+                        style = MaterialTheme.typography.bodyMedium
+                    )
+                }
             }
         }
     }
